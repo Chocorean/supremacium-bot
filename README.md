c0572c2db2d194d51ac7c210c8873f0bd73bd4af
chocorean <baptiste.chocot@gmail.com>
2018-08-30 15:07:29 +0200

    added fully-fonctionnal \!clan command

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # supremacium-bot
-A discord bot for Supremacium Clash Royale clan
+Un bot Discord destiné aux membres du clan Supremacium (Clash Royale)
+
+## Utilisation
+
+Le bot ne répond qu'aux messages privés. On peut intéragir avec lui grâce à plusieurs commandes :
+* !clan [tag] Si exécutée sans argument, cette commande donne des informations sur le clan Supremacium. Si un tag est passé en paramètre, les informations affichées seront celles du clan ayant ce tag. Si le tag n'existe pas, un message d'erreur est affiché.
+
+## Administration
+
+Des commandes supplémentaires seront disponibles pour les chefs adjoints et le chef.
+
+## Détails des commandes
+
+1. **!clan [*tag*]**
+    * Si la commande est exécutée sans argument, le bot renvoie des informations sur notre clan.
+    * Si la commande est exécutée avec un *tag*, le bot renvoie des informations sur le clan ayant le *tag* renseigné.
--- start.py
+++ start.py
@@ -4,8 +4,9 @@ import requests
 import json
 
 TOKEN = 'NDgzMDIzNzA4MDI2NTY4NzA0.DmNmsQ.XwADZhbxNb3XGNeoG6PvIH4d8hw'
-url = "https://api.royaleapi.com/clan/2UOQ98J8"
-
+urlClan = "https://api.royaleapi.com/clan/"
+urlJoueur = "https://api.royaleapi.com/player/"
+CLAN_DEFAUT='2UOQ98J8'
 
 headers = {
     'auth': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTU2NSwiaWRlbiI6IjI2NzY4MTcxOTUzOTIwNDA5NyIsIm1kIjp7fSwidHMiOjE1MzUyMzc5MjI0NTl9.CJp7Ezu6ylH7FMZBB2EoeLidD5ZB983ZvKSC5LXBVbg"
@@ -13,20 +14,54 @@ headers = {
 
 client = discord.Client()
 
+# fonctions utilitaires
+def suiviClan(data):
+    msg = ""
+    msg += "Tag: "           + data['tag']         + "\n"
+    msg += "Description: "   + data['description'] + "\n"
+    msg += "Score du clan: " + str(data['score'])       + "\n"
+    msg += "Membres: "       + str(data["memberCount"]) + "/50\n\n"
+    # top 5
+    msg += "----- Top 5 des membres: -----\n"
+    for i in range(0,5):
+        membre = data['members'][i]
+        msg += "    #" + str(i+1) + " " + membre['name'] + " ("+ str(membre['tag']) + ") avec " + str(membre['trophies']) + " trophés.\n"
+    return msg
+
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+    
+    # ne répond qu'aux messages privés
+    if isinstance(message.channel, discord.channel.PrivateChannel) == False:
+        print('Message ignoré')
+        return
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-    elif message.content.startswith('!test'):
-        response = requests.request("GET", url, headers=headers)
-        data = response.json()
-        msg = data['name'].format(message)
-        await client.send_message(message.channel, msg)
+    msg=""
+    # commande !clan [tag]
+    if message.content.startswith('!clan'):
+        tab = message.content.split()
+        length = len(tab)
+        if length == 1: # pas d'argument
+            response = requests.request("GET", urlClan+CLAN_DEFAUT, headers=headers)
+            data = response.json()
+            msg += suiviClan(data)
+        elif length == 2: # 1 argument
+            response = requests.request("GET", urlClan+tab[1], headers=headers)
+            if response.status_code == 200:
+                data = response.json()
+                msg += suiviClan(data)
+            else:
+                msg += "Clan non trouvé (tag non existant)\n"
+        else: # trop d'argument
+            msg += "usage: `!clan [tag]`\n"
+    else:
+        msg = 'Désolé, je ne parle pas encoe le gobelin.'
+    # envoi
+    msg = msg.format(message)
+    await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
